9bb809b1a17480688556ee28238c7b3357b6571e
Nicholas Hunt-Walker <nhuntwalker@gmail.com>
2017-11-27 12:00:39 -0800

    Updating with a new media directory

---
+++ lending_library/lender_books/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+import datetime
+
+
+class Book(models.Model):
+    cover_image = models.ImageField(upload_to='images')
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+
+    YEARS = [(year, year)
+         for year in range(1, datetime.datetime.now().year + 1)][::-1]
+    year = models.IntegerField(choices=YEARS, default=2016)
+
+    BOOK_STATUS = [
+        ("available", "Available"),
+        ("checked out", "Checked Out")
+    ]
+    status = models.CharField(
+        max_length=20,
+        choices=BOOK_STATUS,
+        default="available"
+    )
+    date_added = models.DateTimeField(auto_now_add=True)
+    last_borrowed = models.DateTimeField(auto_now=True)
